9e83d810b30411fa1f4c4adc672bf0b6dc260278
bosslife19 <wokodavid001@gmail.com>
2024-12-03 08:21:47 +0000

    first production test

--- screens/onBoarding/onBoarding.jsx
+++ screens/onBoarding/onBoarding.jsx
@@ -1,26 +1,47 @@
 import { View, Text, StyleSheet, Image, StatusBar } from 'react-native'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { router } from 'expo-router';
 import { AuthContext } from '@/context/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
  
 const  OnBoardingScreen =()=> {
         const { userDetails} = useContext(AuthContext);
+        const [onboarding, setOnboardingVal] = useState();
+        const setOnboarding = async ()=>{
+          const item = await AsyncStorage.getItem('onboarding');
+          if(item && item=='completed'){
+            setOnboardingVal(item);
+            return;
+          }else{
+            await AsyncStorage.setItem('onboarding', 'started');
+            const value = await AsyncStorage.getItem('onboarding');
+            setOnboardingVal(value);
+          }
+          
+        }
        
-     
-      
+        setOnboarding();
+
+       
     useEffect(()=>{
+      
         setTimeout(() => {
-           
+          
             if(userDetails){
               
               router.push('/(tabs)/home');
-            }else{
-              router.push("/login")
             }
-            // router.push('/(routes)/Welcome-intro');
+            if(onboarding && onboarding =='started'){
+                         router.push('/(routes)/Welcome-intro');
+            }else if(onboarding && onboarding =='completed'){
+              return router.push('/login');
+            }
+            
+            
+
             
          }, 2000);
-    },[])
+    },[onboarding])
 
   return (
     <View style={styles.background}>
