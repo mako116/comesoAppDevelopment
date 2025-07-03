6dfe243fe6f772ad795c8f91bd433bf1c83e1224
mako <nwakwueemmanuel1@gmail.com>
2025-07-03 06:58:09 -0700

    m

--- app/(tabs)/send/index.tsx
+++ app/(tabs)/send/index.tsx
@@ -1,16 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-<<<<<<< HEAD:app/(tabs)/send/index.tsx
-import DashboardScreen from '@/components/Dashboard/Dashboard';
-
-const Send = () => {
-  return (
-   <DashboardScreen/>
-  )
-}
-
-export default Send ;
-=======
 import Searchsection from '../../../screens/Search/Searchsection'
 
 const Search = () => {
@@ -20,6 +9,5 @@ const Search = () => {
 };
 
 export default Search
->>>>>>> ca172e4604f7c42285ba23da255515c37d04301d:app/(tabs)/search/index.jsx
 
 const styles = StyleSheet.create({})
